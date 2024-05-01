196c9b149671da07f139a67c64c0482ff35e8c5a
Yuvasri1405 <131667406+Yuvasri1405@users.noreply.github.com>
2024-05-01 17:14:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from datetime import date
+from prophet import Prophet
+from prophet.plot import plot_plotly
+import plotly.graph_objs as go
 
 START = "2015-02-01"
 TODAY = date.today().strftime("%Y-%m-%d")
