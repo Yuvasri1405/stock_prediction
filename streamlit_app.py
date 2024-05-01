e6126dc808475ee8fac94bc62b330bd22650ac47
Yuvasri1405 <131667406+Yuvasri1405@users.noreply.github.com>
2024-05-01 17:21:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from datetime import date
 
+import yfinance as yf
+from prophet import Prophet
+from prophet.plot import plot_plotly
+import plotly.graph_objs as go
 
 
 START = "2015-02-01"
